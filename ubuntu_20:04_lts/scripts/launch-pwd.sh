17690beeaa481dbc239cb08a011500f338eacd99
NOUMSI ULRICH STEVE <38357057+ulrich-sun@users.noreply.github.com>
2024-11-19 04:01:21 +0100

    update

--- ubuntu_20:04_lts/scripts/launch-pwd.sh
+++ ubuntu_20:04_lts/scripts/launch-pwd.sh
@@ -7,7 +7,7 @@ sudo sed -i "s/localhost/$host_ip/g" config/config.go
 sudo modprobe xt_ipvs
 docker swarm init
 docker pull franela/dind 
-docker-compose up
+docker compose up -d 
 
 echo "Play with Docker is ready to use"
 echo "You can access to $host_ip:80"
